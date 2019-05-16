35d445717a1f5a15bae15d92fe0bcea7174aa8b8
ari.thomazini <ari.thomazini@gmail.com>
2019-05-15 21:07:34 -0300

    Teste css + html

--- src/main/java/sp/senac/br/teste/TesteApplication.java
+++ src/main/java/sp/senac/br/teste/TesteApplication.java
@@ -2,20 +2,11 @@ package sp.senac.br.teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
-@EnableWebMvc
 public class TesteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TesteApplication.class, args);
     }
-
 }
